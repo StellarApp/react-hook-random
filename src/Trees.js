363603a7a11eb373fb5644c41287207912cf2664
StellarApp <49591287+StellarApp@users.noreply.github.com>
2021-10-06 21:56:05 -0500

    trees

--- src/Trees.js
+++ src/Trees.js
@@ -2,7 +2,15 @@ import { useContext } from "react";
 import { TreesContext } from "./";
 
 export default function Trees() {
-  const result = useContext(TreesContext);
-  console.log(result);
-  return <div>tree</div>;
+  const { trees } = useContext(TreesContext);
+  return (
+    <div>
+      <h1> Trees I have </h1>
+      <ul>
+        {trees.map((tree) => (
+          <li key={tree.id}>{tree.type}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
